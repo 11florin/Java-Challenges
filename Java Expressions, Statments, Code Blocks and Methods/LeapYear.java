c3447ad1fe4bad3b8259685327a4d465d621ab11
11florin <albuflorin929@yahoo.co.uk>
2025-07-10 13:27:08 +0100

    Leap year

---
+++ Java Expressions, Statments, Code Blocks and Methods/LeapYear.java
@@ -0,0 +1,25 @@
+public class LeapYear {
+    public static void main(String[] args) {
+        System.out.println("Is leap year: " + isLeapYear(2134));
+        
+    }
+    public static boolean isLeapYear(int year) {
+        if (year < 1 || year > 9999) {
+            return false;
+        }
+        if (year % 4 == 0) {
+            if (year % 100 != 0) {
+                return true;
+            } else {
+                if (year % 400 == 0) {
+                    return true;
+                } else {
+                    return false;
+                }
+            }
+        } else {
+            return false;
+        }
+    }
+}
+
