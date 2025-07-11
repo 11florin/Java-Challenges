9ad0853f1f7299fbee8387ed63f6a01f68828529
11florin <albuflorin929@yahoo.co.uk>
2025-07-11 19:53:40 +0100

    Integer Equality Printer

---
+++ Java Expressions, Statments, Code Blocks and Methods/IntEqualityPrinter.java
@@ -0,0 +1,26 @@
+public class IntEqualityPrinter {
+    private static final String INVALID_VALUE_MESSAGE = "Invalid Value";
+    private static final String ALL_EQUAL_MESSAGE = "All numbers are equal";
+    private static final String NEITHER_ALL_EQUAL_MESSAGE = "Neither all are equal or different";
+    private static final String ALL_DIFF_MESSAGE = "All numbers are different";
+
+    public static void main(String[] args) {
+        printEqual(34, 34, 34);
+        printEqual(34, 33, 33);
+        printEqual(1, 2, 3);
+        printEqual(-1, 1, 1);
+    }
+    public static void printEqual(int a, int b, int c) {
+        
+        if (a < 0 || b < 0 || c < 0) {
+            System.out.println(INVALID_VALUE_MESSAGE);
+        } else if (a == b && b == c) {
+            System.out.println(ALL_EQUAL_MESSAGE);
+        } else if ((a == b) || (a == c) || (b == c)) {
+            System.out.println(NEITHER_ALL_EQUAL_MESSAGE);
+        } else {
+            System.out.println(ALL_DIFF_MESSAGE);
+        }
+    }
+
+}
