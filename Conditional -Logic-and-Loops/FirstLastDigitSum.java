ec997d7add38512bab8d337a58a4ba88f1e4a199
11florin <albuflorin929@yahoo.co.uk>
2025-07-14 14:36:46 +0100

    First and Last Digit Sum

---
+++ Conditional -Logic-and-Loops/FirstLastDigitSum.java
@@ -0,0 +1,17 @@
+public class FirstLastDigitSum {
+    public static void main(String[] args) {
+        System.out.println(sumFirstAndLastDigit(252));
+        System.out.println(sumFirstAndLastDigit(34543));
+    }
+    public static int sumFirstAndLastDigit(int number) {
+        if (number < 0) {
+            return -1;
+        }
+        int lastDigit = number % 10;
+        while (number >= 10) {
+            number /= 10;
+        }
+        int firstDigit = number;
+        return firstDigit + lastDigit;
+    }
+}
