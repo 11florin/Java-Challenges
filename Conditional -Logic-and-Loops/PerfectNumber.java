650bd7d6be60925041a9fb80d42aee6ba0fd7e9e
11florin <albuflorin929@yahoo.co.uk>
2025-07-15 12:48:45 +0100

    Perfect Number

---
+++ Conditional -Logic-and-Loops/PerfectNumber.java
@@ -0,0 +1,23 @@
+public class PerfectNumber {
+    public static void main(String[] args) {
+        System.out.println(isPerfectNumber(6));
+        System.out.println(isPerfectNumber(28));
+        System.out.println(isPerfectNumber(5));
+        System.out.println(isPerfectNumber(-1));
+    }
+    //The method must calculate if the number is perfect. If the number is perfect, 
+    //the method should return true; otherwise, it should return false.
+
+    public static boolean isPerfectNumber(int number) {
+        if (number < 1) {
+            return false;
+        }
+        int sum = 0;
+        for (int i = 1; i < number; i++) {
+            if (number % i == 0) {
+                sum += i;
+            }
+        }
+        return sum == number;
+    }
+}
