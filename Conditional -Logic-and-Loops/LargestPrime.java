f41fdd0cf428b9fb2a74f6239154d3d958d65f98
11florin <albuflorin929@yahoo.co.uk>
2025-07-15 19:56:41 +0100

    Largest Prime

---
+++ Conditional -Logic-and-Loops/LargestPrime.java
@@ -0,0 +1,28 @@
+public class LargestPrime {
+    public static void main(String[] args) {
+        System.out.println(getLargestPrime(21));
+        System.out.println(getLargestPrime(217));
+        System.out.println(getLargestPrime(0));
+        System.out.println(getLargestPrime(45));
+        System.out.println(getLargestPrime(-1));
+    }
+
+    // The method should calculate the largest prime factor of a given number and return it.
+
+    public static int getLargestPrime(int number) {
+        if (number < 2) {
+            return -1;
+        }
+        int factor = -1;
+        for (int i = 2; i * i <= number; i++) {
+            if (number % i != 0) {
+                continue;
+            }
+            factor = i;
+            while (number % i == 0) {
+                number /= i;
+            }
+        }
+        return number == 1 ? factor : number;
+    }
+}
