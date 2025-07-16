6baec271b3b9fa920e11a447189a78debf0e4fea
11florin <albuflorin929@yahoo.co.uk>
2025-07-16 13:53:25 +0100

    Paint Job

---
+++ Conditional -Logic-and-Loops/PaintJob.java
@@ -0,0 +1,39 @@
+public class PaintJob {
+    public static void main(String[] args) {
+        System.out.println(getBucketCount(-3.4, 2.1, 1.5, 2));
+        System.out.println(getBucketCount(3.4, 2.1, 1.5, 2));
+        System.out.println(getBucketCount(2.75, 3.25, 2.5, 1));
+    }
+
+    // Bob is a wall painter and he needs your help. You have to write a program that helps Bob calculate how many 
+    // buckets of paint he needs to buy before going to work. Bob might also have some extra buckets at home. 
+    // He also knows the area that he can cover with one bucket of paint.
+    
+
+    public static int getBucketCount(double width, double height, double areaPerBucket, int extraBuckets) {
+        if (width <= 0 || height <= 0 || areaPerBucket <= 0 || extraBuckets < 0) {
+            return -1;
+        }
+        double area = width * height;
+        double remainingArea = area - extraBuckets * areaPerBucket;
+
+        return getBucketCount(remainingArea, areaPerBucket);
+    }
+
+    public static int getBucketCount(double width, double height, double areaPerBucket) {
+        if (width <= 0 || height <= 0 || areaPerBucket <= 0) {
+            return -1;
+        }
+        double area = width * height;
+        return getBucketCount(area, areaPerBucket);
+    }
+
+    public static int getBucketCount(double area, double areaPerBucket) {
+
+        if (area <= 0 || areaPerBucket <= 0) {
+            return -1;
+        }
+        double numberOfBuckets = area / areaPerBucket;
+        return (int) Math.ceil(numberOfBuckets);
+    }
+}
